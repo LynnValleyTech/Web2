4a39447e1ed6d8861c9624cac05edb93d42bc1b8
LynnValleyTech <lynnvalleytech@gmail.com>
2025-02-04 13:18:03 -0800

    Create About.js

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+
+const About = () => {
+  return (
+    <section className="about">
+      <h2>About Me</h2>
+      <p>I am a full-stack developer with experience in React, Node.js, and more.</p>
+    </section>
+  );
+};
+
+export default About;
