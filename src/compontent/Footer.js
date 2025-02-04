10f76b03fcf83c46584e470f7ee9fa4e442df362
LynnValleyTech <lynnvalleytech@gmail.com>
2025-02-04 13:18:55 -0800

    Create Footer.js

---
+++ src/compontent/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; 2023 My Portfolio. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
